483fa317b54b328f6673ca481468f127f5c8d049
chojangg <s2115@e-mirim.hs.kr>
2023-06-14 12:09:42 +0900

    새 폼 생성 및 모달리스로 띄우기

--- ModalModelessPrac/Form1.cs
+++ ModalModelessPrac/Form1.cs
@@ -12,6 +12,15 @@ namespace ModalModelessPrac
 {
     public partial class Form1 : Form
     {
+        class CustomForm : Form
+        {
+            public CustomForm()
+            {
+                this.Text = "제목 글자";
+            }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
@@ -26,11 +35,17 @@ namespace ModalModelessPrac
             {
                 result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel);
             } while (result == DialogResult.Retry);
-            if (result == DialogResult.Cancel)
+            if(result == DialogResult.Cancel)
             {
                 MessageBox.Show("사용자가 취소하였습니다.", "알림",
                     MessageBoxButtons.OK, MessageBoxIcon.Hand);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CustomForm form = new CustomForm();
+            form.Show();
         }
     }
 }
